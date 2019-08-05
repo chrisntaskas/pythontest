01c6fc6fa1bf88c50ec428a333cffdf7daca0b8c
Chris <48050599+chrisntaskas@users.noreply.github.com>
2019-08-05 13:17:52 +0200

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,4 @@
-print("hello")
+username = str(input("What's your name?"))
+age = int(input("Please enter your age"))
+days = 365 * age
+print("Hello " + username + " , you have beenn alive for " + str(days) + " days")
